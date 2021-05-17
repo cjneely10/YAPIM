77a7ae12e474b455eaa8610afa3ee980aafc4436
Christopher Neely <christopher.neely1200@gmail.com>
2021-05-17 14:24:05 -0700

    Move around

--- src/executor.py
+++ src/executor.py
@@ -3,7 +3,7 @@ import pickle
 from pathlib import Path
 from typing import List, Dict, Type, Optional
 
-from src.task_distributor import TaskDistributor
+from src.utils.task_distributor import TaskDistributor
 from src.tasks.task import Task
 from src.tasks.utils.dependency_graph import Node, DependencyGraph
 from src.utils.config_manager import ConfigManager
