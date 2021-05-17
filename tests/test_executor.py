8ecc1d4669d5668fe9fd62249f61f8fb029f6db8
Christopher Neely <christopher.neely1200@gmail.com>
2021-05-17 13:31:03 -0700

    Add FASTA loader

--- tests/test_executor.py
+++ tests/test_executor.py
@@ -8,21 +8,22 @@ from src.utils.input_loader import InputLoader
 
 
 class TestExecutor(TestCase):
-    sample_tasks1_dir = Path(os.path.dirname(__file__)).resolve().joinpath("sample_tasks1")
-    sample_dependencies_dir = Path(os.path.dirname(__file__)).resolve().joinpath("sample_dependencies")
-    sample_config_file = sample_tasks1_dir.joinpath("sample-config.yaml")
 
-    class DefaultLoader(InputLoader):
+    def test_simple(self):
+        sample_tasks1_dir = Path(os.path.dirname(__file__)).resolve().joinpath("sample_tasks1")
+        sample_dependencies_dir = Path(os.path.dirname(__file__)).resolve().joinpath("sample_dependencies")
+        sample_config_file = sample_tasks1_dir.joinpath("sample-config.yaml")
 
-        def load(self) -> Dict[str, Dict]:
-            return {str(i): {} for i in range(10)}
+        class DefaultLoader(InputLoader):
 
-    def test_run(self):
+            def load(self) -> Dict[str, Dict]:
+                return {str(i): {} for i in range(10)}
+
         executor = Executor(
-            TestExecutor.DefaultLoader(),
-            TestExecutor.sample_config_file,
+            DefaultLoader(),
+            sample_config_file,
             Path(os.path.join(os.path.dirname(__file__), "out")).resolve(),
-            TestExecutor.sample_tasks1_dir,
-            [TestExecutor.sample_dependencies_dir]
+            sample_tasks1_dir,
+            [sample_dependencies_dir]
         )
         executor.run()
