cba7343dac7362cfe66c7b66bc1d54beb8eacc44
Christopher Neely <christopher.neely1200@gmail.com>
2021-05-17 13:30:06 -0700

    Add FASTA loader

---
+++ src/fasta_loader.py
@@ -0,0 +1,25 @@
+import os
+from pathlib import Path
+from typing import Dict, Set
+
+from Bio import SeqIO
+from src.utils.input_loader import InputLoader
+
+
+class FastaLoader(InputLoader):
+    def __init__(self, fasta_directory: Path, write_dir: Path, valid_extensions: Set[str]):
+        self.fasta_directory = fasta_directory
+        self.valid_extensions = valid_extensions
+        self.write_directory = write_dir
+
+    def load(self) -> Dict[str, Dict]:
+        out = {}
+        for file in os.listdir(self.fasta_directory):
+            file = os.path.join(self.fasta_directory, file)
+            for ext in self.valid_extensions:
+                if ext in file:
+                    basename = os.path.basename(os.path.splitext(file)[0])
+                    new_file = os.path.join(self.write_directory, basename + ".fna")
+                    SeqIO.write(SeqIO.parse(file, "fasta"), new_file, "fasta")
+                    out[basename] = {"fasta": new_file}
+        return out
--- src/utils/input_loader.py
+++ src/utils/input_loader.py
@@ -3,9 +3,6 @@ from typing import Dict
 
 
 class InputLoader(ABC):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
     @abstractmethod
     def load(self) -> Dict[str, Dict]:
         """
