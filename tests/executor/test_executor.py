434933d4a561ae006c59a945586eb7b3f4ec6097
Christopher Neely <christopher.neely1200@gmail.com>
2021-05-17 17:12:37 -0700

    Begin for more dynamic loading locations

--- tests/executor/test_executor.py
+++ tests/executor/test_executor.py
@@ -20,11 +20,11 @@ class TestExecutor(TestCase):
                 return {str(i): {} for i in range(self.n)}
 
         executor = Executor(
-            TestLoader(10),
-            sample_config_file,
-            Path(os.path.join(os.path.dirname(__file__), "out")).resolve(),
-            "sample_tasks1",
-            ["sample_dependencies"]
+            TestLoader(10),  # Input loader
+            sample_config_file,  # Config file path
+            Path(os.path.join(os.path.dirname(__file__), "out")).resolve(),  # Base output dir path
+            "simple/sample_tasks1",  # Relative path to pipeline directory
+            ["simple/sample_dependencies"]  # List of relative paths to dependency directories
         )
         executor.run()
 
