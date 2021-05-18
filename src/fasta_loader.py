939e09eb0cd249f18175868a02fc2db9810bc59c
Christopher Neely <christopher.neely1200@gmail.com>
2021-05-17 21:25:38 -0700

    Parallelize FASTA input loading

--- src/fasta_loader.py
+++ src/fasta_loader.py
@@ -1,6 +1,7 @@
 import os
+from concurrent.futures import ThreadPoolExecutor, as_completed
 from pathlib import Path
-from typing import Dict, Set
+from typing import Dict, Set, Tuple
 
 from Bio import SeqIO
 
@@ -17,17 +18,25 @@ class FastaLoader(InputLoader):
 
     def load(self) -> Dict[str, Dict]:
         out = {}
-        for file in os.listdir(self.fasta_directory):
-            ext = os.path.splitext(file)[1]
-            if ext in self.valid_extensions:
-                file = os.path.join(self.fasta_directory, file)
-                basename = os.path.basename(os.path.splitext(file)[0])
-                new_file = os.path.join(self.write_directory, basename + ".fna")
-                if not os.path.exists(new_file):
-                    records = []
-                    for record in SeqIO.parse(file, "fasta"):
-                        record.description = ""
-                        records.append(record)
-                    SeqIO.write(records, new_file, "fasta")
-                out[basename] = {"fasta": new_file}
+        with ThreadPoolExecutor() as executor:
+            futures = []
+            for file in os.listdir(self.fasta_directory):
+                ext = os.path.splitext(file)[1]
+                if ext in self.valid_extensions:
+                    futures.append(executor.submit(self._load_file, file))
+            for future in as_completed(futures):
+                result = future.result()
+                out[result[0]] = result[1]
         return out
+
+    def _load_file(self, file: str) -> Tuple[str, dict]:
+        file = os.path.join(self.fasta_directory, file)
+        basename = os.path.basename(os.path.splitext(file)[0])
+        new_file = os.path.join(self.write_directory, basename + ".fna")
+        if not os.path.exists(new_file):
+            records = []
+            for record in SeqIO.parse(file, "fasta"):
+                record.description = ""
+                records.append(record)
+            SeqIO.write(records, new_file, "fasta")
+        return basename, {"fasta": new_file}
