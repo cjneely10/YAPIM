6c98c29bd713dcc880601e2fbe40027c6b5e7b68
Christopher Neely <christopher.neely1200@gmail.com>
2021-05-18 19:27:00 -0700

    Refactor: update actual input to task with new dependency name information

--- src/tasks/utils/dependency_input.py
+++ src/tasks/utils/dependency_input.py
@@ -4,9 +4,6 @@ Module holds DependencyInput `struct` that is used to in TaskList.depends member
 
 from typing import Dict, Optional
 
-# pylint: disable=too-few-public-methods
-from src.utils.config_manager import ConfigManager
-
 
 class DependencyInput:
     """ Class handles parsing input for dependency requirements
