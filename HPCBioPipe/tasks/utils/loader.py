15197103a2a454128fd92e26a4234bde04baeca0
Christopher Neely <christopher.neely1200@gmail.com>
2021-05-22 13:57:07 -0700

    Simplify loader search

--- HPCBioPipe/tasks/utils/loader.py
+++ HPCBioPipe/tasks/utils/loader.py
@@ -1,42 +1,16 @@
 """
 Populate dependencies for easy loading
 """
-import os
-from importlib import import_module
+import pkgutil
 from inspect import isclass
-from pkgutil import iter_modules
 
 from HPCBioPipe.tasks.task import Task
 
 
 def get_modules(package_dir: str) -> dict:
-    """ Walk directory for loadable modules
-
-    :param package_dir:
-    :return:
-    """
-    all_modules = {}
-    for directory, _, _ in os.walk(package_dir, followlinks=True):
-        all_modules.update(_get_modules_at_dir(directory))
-    return all_modules
-
-
-def _get_modules_at_dir(package_dir: str):
-    """ Dynamically load all modules contained at sublevel
-
-    :param package_dir: Directory from location where this function is called
-    :return: Dict of task.name: Task object class
-    """
-    # iterate through the modules in the current package
     out = {}
-    for (_, module_name, _) in iter_modules([package_dir]):
-        # import the module and iterate through its attributes
-        module = import_module(
-            "{}.{}".format(
-                package_dir.replace("/", "."),
-                module_name
-            )
-        )
+    for loader, module_name, is_pkg in pkgutil.walk_packages([package_dir]):
+        module = loader.find_module(module_name).load_module(module_name)
         for attribute_name in dir(module):
             attribute = getattr(module, attribute_name)
             if isclass(attribute) and issubclass(attribute, Task) and "Task" not in attribute.__name__:
