1f8f4c181613c48ce7db50d6e4381088f3ed041b
Christopher Neely <christopher.neely1200@gmail.com>
2021-05-25 10:25:49 -0700

    Remove naming requirement for task classes

--- yapm/tasks/utils/loader.py
+++ yapm/tasks/utils/loader.py
@@ -2,7 +2,7 @@
 Populate dependencies for easy loading
 """
 import pkgutil
-from inspect import isclass
+from inspect import isclass, isabstract
 from pathlib import Path
 
 from yapm.tasks.task import Task
@@ -14,7 +14,7 @@ def get_modules(package_dir: Path) -> dict:
         module = loader.find_module(module_name).load_module(module_name)
         for attribute_name in dir(module):
             attribute = getattr(module, attribute_name)
-            if isclass(attribute) and issubclass(attribute, Task) and "Task" not in attribute.__name__:
+            if isclass(attribute) and issubclass(attribute, Task) and not isabstract(attribute):
                 # Add the class to this package's variables
                 out[attribute.__name__] = attribute
     return out
