738bd1da4c83725a6ea51564577a77ba0d8fe218
Christopher Neely <christopher.neely1200@gmail.com>
2021-06-07 16:22:08 -0700

    Finish refactor with new pipeline loader

--- yapim/utils/package_generator.py
+++ yapim/utils/package_generator.py
@@ -5,6 +5,7 @@ from pathlib import Path
 from typing import List, Optional
 
 from yapim import InputLoader
+from yapim.utils.config_manager_generator import ConfigManagerGenerator
 from yapim.utils.package_manager import PackageManager
 
 
@@ -13,9 +14,9 @@ class PackageGenerator(PackageManager):
                  tasks_directory: Path,
                  dependencies_directories: Optional[List[Path]],
                  input_loader_path: Optional[Path] = None):
-        self._tasks_directory = os.path.basename(tasks_directory)
+        self._tasks_directory = tasks_directory
         if input_loader_path is not None and PackageGenerator._loader_type_is_valid(str(input_loader_path)):
-            self._loader = os.path.basename(input_loader_path)
+            self._loader = input_loader_path
         else:
             self._loader = None
         if dependencies_directories is not None:
@@ -28,7 +29,7 @@ class PackageGenerator(PackageManager):
         if not write_directory.exists():
             os.makedirs(write_directory)
         output_data = {
-            "loader": self._loader,
+            "loader": (os.path.basename(self._loader) if self._loader is not None else None),
             "dependencies": [
                 os.path.basename(dependency_directory)
                 for dependency_directory in self._dependencies_directories
@@ -40,18 +41,26 @@ class PackageGenerator(PackageManager):
         if not write_directory.exists():
             os.makedirs(write_directory)
         # Copy pipeline contents to own directory
-        shutil.copytree(self._tasks_directory, output_data["tasks"], symlinks=True)
+        shutil.copytree(self._tasks_directory, write_directory.joinpath(output_data["tasks"]),
+                        symlinks=True, dirs_exist_ok=True)
         # Copy loader
         if self._loader is not None:
             shutil.copy(self._loader, write_directory)
         # Copy all dependency directories
         for pre, post in zip(self._dependencies_directories, output_data["dependencies"]):
-            shutil.copytree(pre, write_directory.joinpath(post), symlinks=True)
+            shutil.copytree(pre, write_directory.joinpath(post), symlinks=True, dirs_exist_ok=True)
         # Save metadata file
         pipeline_file = write_directory.joinpath(PackageGenerator.pipeline_file)
         if not pipeline_file.exists() or input("Overwrite existing pipeline file? [Y/n]: ").upper() == "Y":
             with open(pipeline_file, "wb") as file_ptr:
                 pickle.dump(output_data, file_ptr)
+        # Create config file
+        self._create_config(write_directory)
+
+    def _create_config(self, write_directory: Path):
+        config_file_path = write_directory.joinpath(os.path.basename(self._tasks_directory) + "-config.yaml")
+        if not config_file_path.exists() or input("Overwrite existing configuration file? [Y/n]: ").upper() == "Y":
+            ConfigManagerGenerator(self._tasks_directory, self._dependencies_directories).write(config_file_path)
 
     @staticmethod
     def _loader_type_is_valid(loader_name: str) -> bool:
