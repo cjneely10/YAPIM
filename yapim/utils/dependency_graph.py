3b320d0b899ff0ab54df6a91287898f1f4ea37a7
Christopher Neely <christopher.neely1200@gmail.com>
2021-06-08 10:51:59 -0700

    Finish adding affected node collection, begin testing

--- yapim/utils/dependency_graph.py
+++ yapim/utils/dependency_graph.py
@@ -1,8 +1,7 @@
 import inspect
-from typing import List, Dict, Tuple, Type, Iterable, Optional
+from typing import List, Dict, Tuple, Type, Iterable, Optional, Set
 
 import networkx as nx
-from networkx import DiGraph, topological_sort, is_directed_acyclic_graph
 
 from yapim import AggregateTask
 from yapim.tasks.task import Task
@@ -68,17 +67,19 @@ class DependencyGraph:
         """
         self.idx: Dict[str, Type[Task]] = task_blueprints
 
-        self.graph = DiGraph()
-        self.graph.add_node(DependencyGraph.ROOT_NODE)
+        self._graph = nx.DiGraph()
+        self._graph.add_node(DependencyGraph.ROOT_NODE)
         self._build_dependency_graph(tasks)
-        if not is_directed_acyclic_graph(self.graph):
+        self._sorted_graph: Optional[List[List[Node]]] = None
+        if not nx.is_directed_acyclic_graph(self._graph):
             print("Cycle found!")
             raise DependencyGraph.ERR
+        self.sort_graph()
 
     def _build_dependency_graph(self, tasks: Iterable[Type[Task]]):
         for task in tasks:
             task_node: Node = Node(DependencyGraph.ROOT, task.__name__)
-            self.graph.add_edge(DependencyGraph.ROOT_NODE, task_node)
+            self._graph.add_edge(DependencyGraph.ROOT_NODE, task_node)
             # Link requirements for already completed tasks in pipeline
             # Gather dependencies needed for fulfilling given requirement
             if task.requires() is None:
@@ -96,7 +97,7 @@ class DependencyGraph:
                 if requirement not in self.idx.keys():
                     print(f"Unable to locate {requirement}")
                     raise DependencyGraph.ERR
-                self.graph.add_edge(Node(DependencyGraph.ROOT, requirement), task_node)
+                self._graph.add_edge(Node(DependencyGraph.ROOT, requirement), task_node)
 
     def _add_dependencies(self, task_node: Node, scope: str, graph: nx.Graph):
         # Link dependency names
@@ -119,7 +120,9 @@ class DependencyGraph:
             scope_type = self.idx[dependency.name]
             if issubclass(scope_type, AggregateTask):
                 if not issubclass(task, AggregateTask):
-                    print(f"Task <{task.__name__}> is of type AggregateTask and can only use AggregateTask dependencies")
+                    print(
+                        f"Task <{task.__name__}> is of type AggregateTask and can only use AggregateTask dependencies"
+                    )
                     raise DependencyGraph.ERR
             else:
                 if issubclass(task, AggregateTask):
@@ -129,20 +132,55 @@ class DependencyGraph:
             graph.add_edge(task_node, dependency_node)
             self._add_dependencies(dependency_node, scope, graph)
 
-    @property
-    def sorted_graph_identifiers(self) -> List[List[Node]]:
-        sorted_graph = list(topological_sort(self.graph))
+    def sort_graph(self):
+        sorted_graph = list(nx.topological_sort(self._graph))
         sorted_graph.remove(DependencyGraph.ROOT_NODE)
         out_steps = []
         for node in sorted_graph:
             graph = nx.DiGraph()
             self._add_dependencies(node, node.name, graph)
-            step_list = list(reversed(list(topological_sort(graph))))
+            step_list = list(reversed(list(nx.topological_sort(graph))))
             if len(step_list) > 0:
                 out_steps.append(step_list)
             else:
                 out_steps.append([node])
-        return out_steps
+        self._sorted_graph = out_steps
 
-    def get_affected_nodes(self, task_name: str, dependency_name: Optional[str] = None):
-        pass
+    @property
+    def sorted_graph_identifiers(self) -> List[List[Node]]:
+        return self._sorted_graph
+
+    def _find_task_idx(self, task_name: str, dependency_name: Optional[str] = None) -> Tuple[int, int]:
+        for i, task_list in enumerate(self.sorted_graph_identifiers):
+            if task_list[-1].name == task_name:
+                if dependency_name is None:
+                    return i, len(task_list) - 1
+                else:
+                    for j, task in enumerate(task_list[:-1]):
+                        if task.name == dependency_name:
+                            return i, j
+        raise AttributeError("Unable to locate task name!")
+
+    def _get_tasks_name_strings(self, task_idx: Tuple[int, int]) -> List[str]:
+        task_names: list = [self.sorted_graph_identifiers[task_idx[0]][-1].name]
+        if task_idx[1] != -1:
+            task_names.append(task_names[-1] + "." + self.sorted_graph_identifiers[task_idx[0]][task_idx[1]].name)
+        return task_names
+
+    def _connecting_paths(self, task1: Node, task2: Node):
+        return nx.all_simple_paths(self._graph, task1, task2)
+
+    def get_affected_nodes(self, task_name: str, dependency_name: Optional[str] = None) -> Set[str]:
+        out_nodes = set()
+        self._get_nodes_helper(out_nodes, task_name, dependency_name)
+        return out_nodes
+
+    def _get_nodes_helper(self, out_nodes: Set[str], task_name: str, dependency_name: Optional[str] = None):
+        task_pos = self._find_task_idx(task_name, dependency_name)
+        for task_name in self._get_tasks_name_strings(task_pos):
+            out_nodes.add(task_name)
+        for i, task_list in enumerate(self.sorted_graph_identifiers[task_pos[0] + 1:]):
+            connections = self._connecting_paths(self.sorted_graph_identifiers[task_pos[0]][-1], task_list[-1])
+            if len(connections) > 0:
+                for node in connections:
+                    self._get_nodes_helper(out_nodes, node.name)
