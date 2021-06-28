e720c72113be535c4cb413783bed0999f102343d
Christopher Neely <christopher.neely1200@gmail.com>
2021-06-28 13:10:27 -0700

    Update SLURM to reduce number of times status is checked

--- yapim/tasks/utils/slurm_caller.py
+++ yapim/tasks/utils/slurm_caller.py
@@ -76,7 +76,7 @@ class SLURMCaller:
         """
         # Call script using sbatch
         log_line = str(local["sbatch"][self.script]()).split()
-        print(log_line)
+        print("resp", log_line)
         # If no output to stdout/err, return
         if len(log_line) == 0:
             return
