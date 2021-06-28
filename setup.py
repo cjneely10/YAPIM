7ce0c5e6d294c01cff7709581a1a951e4385472e
Christopher Neely <christopher.neely1200@gmail.com>
2021-06-28 11:36:43 -0700

    Begin debug

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     install_requires=[
         "bcbio-gff>=0.6.6",
         "biopython>=1.76",
-        "plumbum>=1.6.6",
+        "plumbum==1.6.6",
         "networkx==2.2",
         "pyyaml>=5.3.1",
         "art>=5.1",
