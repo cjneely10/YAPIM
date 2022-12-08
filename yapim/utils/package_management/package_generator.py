55e9aa39ff9fa7e63a9ab930579cb4c7a5f869cb
Christopher Neely <christopher.neely1200@gmail.com>
2022-12-08 11:31:55 -0800

    Development branch to ci; Fix package generation bug on WSL

--- yapim/utils/package_management/package_generator.py
+++ yapim/utils/package_management/package_generator.py
@@ -1,10 +1,10 @@
+import errno
 import os
 import pickle
 import shutil
 from pathlib import Path
 from typing import List, Optional
 
-from yapim import InputLoader
 from yapim.utils.package_management.config_manager_generator import ConfigManagerGenerator
 from yapim.utils.package_management.package_manager import PackageManager
 
@@ -15,10 +15,7 @@ class PackageGenerator(PackageManager):
                  dependencies_directories: Optional[List[Path]],
                  input_loader_path: Optional[Path] = None):
         self._tasks_directory = tasks_directory
-        # if input_loader_path is not None and PackageGenerator._loader_type_is_valid(str(input_loader_path)):
         self._loader = input_loader_path
-        # else:
-        #     self._loader = None
         if dependencies_directories is not None:
             self._dependencies_directories = dependencies_directories
         else:
@@ -28,7 +25,7 @@ class PackageGenerator(PackageManager):
         if not write_directory.exists():
             os.makedirs(write_directory)
         output_data = {
-            "loader": (True if self._loader is not None else False),
+            "loader": self._loader is not None,
             "dependencies": [
                 os.path.basename(dependency_directory)
                 for dependency_directory in self._dependencies_directories
@@ -37,14 +34,14 @@ class PackageGenerator(PackageManager):
             "tasks": os.path.basename(self._tasks_directory)
         }
         # Copy pipeline contents to own directory
-        shutil.copytree(self._tasks_directory, write_directory.joinpath(output_data["tasks"]),
-                        symlinks=True, dirs_exist_ok=True)
+        PackageGenerator._try_copy(self._tasks_directory, write_directory.joinpath(output_data["tasks"]),
+                                   symlinks=True, dirs_exist_ok=True)
         # Copy loader
         if self._loader is not None:
             shutil.copy(self._loader, write_directory)
         # Copy all dependency directories
         for pre, post in zip(self._dependencies_directories, output_data["dependencies"]):
-            shutil.copytree(pre, write_directory.joinpath(post), symlinks=True, dirs_exist_ok=True)
+            PackageGenerator._try_copy(pre, write_directory.joinpath(post), symlinks=True, dirs_exist_ok=True)
         # Save metadata file
         pipeline_file = write_directory.joinpath(PackageGenerator.pipeline_file)
         # Create config stuff
@@ -59,3 +56,30 @@ class PackageGenerator(PackageManager):
         config_file_path = write_directory.joinpath(os.path.basename(self._tasks_directory) + "-config.yaml")
         if not config_file_path.exists() or input("Overwrite existing configuration file? [Y/n]: ").upper() == "Y":
             ConfigManagerGenerator(self._tasks_directory, self._dependencies_directories).write(config_file_path)
+
+    @staticmethod
+    def _try_copy(pre: Path, post: Path, *args, **kwargs):
+        """
+        Attempt to copy directory contents.
+
+        Per "https://bugs.python.org/issue38633", `shutil` may raise error on WSL.
+        Apply patch described and reattempt. If fails on this error, will not be caught within function.
+
+        :param pre: Source path
+        :param post: Destination path
+        :raises OSError: If fails to copy despite presence of WSL patch
+        """
+        try:
+            shutil.copytree(pre, post, *args, **kwargs)
+        except shutil.Error:
+            orig_copyxattr = shutil._copyxattr
+
+            def patched_copyxattr(src, dst, *, follow_symlinks=True):
+                try:
+                    orig_copyxattr(src, dst, follow_symlinks=follow_symlinks)
+                except OSError as ex:
+                    if ex.errno != errno.EACCES:
+                        raise
+
+            shutil._copyxattr = patched_copyxattr
+            shutil.copytree(pre, post, *args, **kwargs)
